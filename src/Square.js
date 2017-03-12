d711cc8483bbeea22964e3a5b13a95318ddaa4ab
Tower450 <joe.tourangeau@gmail.com>
2017-03-12 11:51:41 -0400

    infra JSX

---
+++ src/Square.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+export default Square;
